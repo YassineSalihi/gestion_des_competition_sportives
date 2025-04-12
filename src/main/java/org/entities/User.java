725726f929111432cd5d396b42fe0d9f5a1f5bb6
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-04-12 12:09:33 +0100

    test

--- src/main/java/org/entities/User.java
+++ src/main/java/org/entities/User.java
@@ -1,10 +1,10 @@
 package org.entities;
 
-/*import javax.persistence.Entity;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;*/
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
---
+++ src/main/java/util/TestHibernate.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author yassinesalihi
+ */
+public class TestHibernate {
+    
+    {
+        System.out.println("non static 1");
+    }
+    
+    static {
+        System.out.println("static 1");
+    }
+    
+    static {
+        System.out.println("static 2");
+    }
+    
+    
+    {
+        System.out.println("non static 2");
+    }
+
+    public TestHibernate() {
+        System.out.println("construct");
+    }
+    
+    
+    public static void main(String[] args) {
+        new TestHibernate();
+    }
+}
